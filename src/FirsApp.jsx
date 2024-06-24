4e5ceaee5294e9d3ebc2b7129c13cf75dd2cbe86
jagues6 <alexguevara31@gmail.com>
2024-06-23 22:08:43 -0500

    Grid en los tipos de pokemon

--- src/FirsApp.jsx
+++ src/FirsApp.jsx
@@ -5,7 +5,7 @@ import { LinearProgress, Button } from '@mui/material';
 
 export const FirsApp = () => {
 
-    const [imgPokemon, setImgPokemon] = useState({ img: '', stats: [], types: [], name: "", tiposDanio: [] })
+    const [imgPokemon, setImgPokemon] = useState({ img: '', stats: [], types: [], name: "", tiposDanio: [], height: 0, weight: 0 })
     //const [imgPokemon, setImgPokemon] = useState(null)
     const colores = {
         normal: '#A8A77A',
@@ -42,7 +42,9 @@ export const FirsApp = () => {
                 stats: response.data.stats,
                 types: response.data.types,
                 name: response.data.name,
-                id: response.data.id
+                id: response.data.id,
+                height: response.data.height,
+                weight: response.data.weight
             }
             console.log(objPokemon);
             setImgPokemon(objPokemon)
@@ -78,29 +80,34 @@ export const FirsApp = () => {
 
 
     return (
-        <div>
+        <div style={{textAlign: 'center'}}>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <img style={{ height: "100px", width: "200px" }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHPCEu5wYHfkiJzriogDrM2_Tcfus-m1MLVw&s" alt="" />
                 <img style={{ height: "100px", width: "300px" }} src="https://img.genial.ly/5e92f91205731330e40eb30f/1c6f47d4-6372-41ee-a639-e69a530a7bd8.png" alt="" />
                 <img style={{ height: "100px", width: "200px" }} src="https://media.cnn.com/api/v1/images/stellar/prod/210226041654-05-pokemon-anniversary-design.jpg?q=w_1920,h_1080,x_0,y_0,c_fill" alt="" />
             </div>
             <div id="hijo">
-                <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-                    <div style={{ display: 'flex', flexDirection: 'column', borderRadius: "20px", alignItems: 'center', backgroundColor: colores[imgPokemon.types[0]?.type.name] }}>
-                        <h1>{imgPokemon.name.charAt(0).toUpperCase() + imgPokemon.name.slice(1)}</h1>
-                        <img style={{ height: "70%", width: "70%" }} src={imgPokemon.img} alt="" />
-                    </div>
-                    <div>
-                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <h1 style={{fontSize:"100px", fontStyle:"italic", margin:"0", marginBottom:"10px", color:colores[imgPokemon.types[0]?.type.name]}}># {imgPokemon.id}</h1>
+                <div id='card' style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', padding:"10px", borderRadius: "20px", alignItems: 'center', backgroundColor: colores[imgPokemon.types[0]?.type.name] }}>
+                            <h1 style={{ fontFamily:"cursive"}}>{imgPokemon.name.charAt(0).toUpperCase() + imgPokemon.name.slice(1)}</h1>
+                            <img style={{ height: "70%", width: "70%" }} src={imgPokemon.img} alt="" />
+                      
+                        <div style={{ display: 'flex', width: "100%", flexDirection: 'row', justifyContent: 'space-around' }}>
+                            <p style={{ color: "white", margin:"10px 0", fontStyle: "italic", fontSize:"30px"}}>Altura: {imgPokemon.height / 10}m</p>
+                            <p style={{ color: "white", margin:"10px 0", fontStyle: "italic", fontSize:"30px"}}>Altura: {imgPokemon.weight / 10}kg</p>
                         </div>
+                    </div>
+                    <div id="tipos" style={{display:"flex", flexDirection: "column", justifyContent:"center", alignItems: "center", padding:"10px"}}>
                         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <h1 style={{ margin: "0", marginBottom: "20px" }}>Tipos del pokemon</h1>
-                            <div style={{ display: "grid", gridTemplateColumns: `repeat(${imgPokemon.types?.length > 1 ? 3 : 1}, 1fr)`, gap: "10px", justifyContent: 'center' }}>
-                                
+                            <h1 style={{ fontSize: "100px", fontStyle: "italic", margin: "0", marginBottom: "10px", color: colores[imgPokemon.types[0]?.type.name] }}># {imgPokemon.id}</h1>
+                        </div>
+                        <div id='tiposPokemon' style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <h1 style={{ margin: "0", marginBottom: "20px", fontSize:"35px" }}>Tipos del pokemon</h1>
+                            <div style={{ display: "grid", gridTemplateColumns: `repeat(${imgPokemon.types?.length > 1 && imgPokemon.types?.length < 3 ? 2 : imgPokemon.types?.length > 2 ? 3 : 1 }, 1fr)`, gap: "10px", justifyContent: 'center' }}>
+
                                 {
                                     imgPokemon.types.map((e, i) => {
-                                        return <div key={i} style={{ display:"flex", justifyContent: 'center', alignItems:"center" }}>
+                                        return <div key={i} style={{ display: "flex", justifyContent: 'center', alignItems: "center" }}>
                                             <Button sx={{ background: colores[e.type.name] }} variant="contained" size="medium">{e.type.name}</Button>
                                         </div>
 
@@ -108,14 +115,15 @@ export const FirsApp = () => {
                                 }
                             </div>
                         </div>
-                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <h1 style={{ margin: "0", marginBottom: "20px", marginTop: "10px" }}>Debilidades del pokemon</h1>
-                            <div style={{ display: "grid", gridTemplateColumns: `repeat(${imgPokemon.tiposDanio?.length > 1 ? 3 : 1}, 1fr)`, gap: "10px", justifyContent: 'center', alignItems:"center" }}>
+                        <div id='debPokemon' style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <h1 style={{ margin: "0", marginBottom: "20px", marginTop: "10px", fontSize:"35px" }}>Debilidades del pokemon</h1>
+                            <div style={{ display: "grid", gridTemplateColumns: `repeat(${imgPokemon.tiposDanio?.length > 1 && imgPokemon.tiposDanio?.length < 3 ? 2 : imgPokemon.tiposDanio?.length > 2 ? 3 : 1}, 1fr)`, gap: "10px", justifyContent: 'center', alignItems: "center" }}>
+                                
                                 {
                                     !imgPokemon.tiposDanio ?
                                         <p>No hay debilidades pokemon</p> :
                                         imgPokemon.tiposDanio.map((e, i) => {
-                                            return <div key={i} style={{  display:"flex", justifyContent: 'center', alignItems:"center" }}>
+                                            return <div key={i} style={{ display: "flex", justifyContent: 'center', alignItems: "center" }}>
                                                 <Button sx={{ background: colores[e.name] }} variant="contained" size="medium">{e.name}</Button>
                                             </div>
 
@@ -128,6 +136,7 @@ export const FirsApp = () => {
 
                 </div>
                 <div style={{ padding: "20px" }}>
+                    <h1 style={{fontFamily:"cursive", margin:"0"}}>Estadisticas</h1>
                     <div>{imgPokemon.stats.map((element, i) => {
                         return <div key={i}>
                             <p style={{ fontFamily: "sans-serif", fontWeight: "bolder" }}>{element.stat.name.charAt(0).toUpperCase() + element.stat.name.slice(1)} : {element.base_stat}/255</p>
