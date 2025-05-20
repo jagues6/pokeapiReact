847c495e2a84a33cd58792e50bbfb3b4c96b3f12
jagues6 <142562482+SENACAT@users.noreply.github.com>
2025-05-20 16:33:37 -0500

    Update Citas.jsx

--- src/pages/Citas Veterinaria/Citas.jsx
+++ src/pages/Citas Veterinaria/Citas.jsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material';
+import DialogContentText from '@mui/material/DialogContentText';
 //import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
@@ -102,6 +102,10 @@ function Citas() {
 
     const handleClose3 = () => {
         setOpenAlert2(false)
+    }
+
+       const handleClose4 = () => {
+        setOpenAlert3(false)
     }
 
     const saveData = () => {
